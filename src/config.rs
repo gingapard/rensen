2796ae00f7fb659d8a405f007ec14be488387348
bampham <cbroo@duck.com>
2024-03-20 20:50:05 +0100

    config
    added serde for yaml which can maybe replace json

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,6 @@
 use serde::{Serialize, Deserialize};
 use serde_json;
+use serde_yaml;
 use std::fs::File;
 use std::path::PathBuf;
 use std::io::{prelude::*, self, Write, Read};
@@ -24,6 +25,9 @@ pub struct HostConfig {
 }
 
 impl HostConfig {
+    /// Constructor method for HostConfig not actually
+    /// a part of the program, but just for generating 
+    /// the config file in unit tests.
     pub fn new(
         user: String,
         identifier: HostIdentifier,
@@ -53,6 +57,7 @@ impl Settings {
         Self {hosts}
     }
 
+    // json
     pub fn serialize_json(&self, file_path: &str) -> std::io::Result<()> {
         let mut file = File::create(file_path)?;
         let json_str = serde_json::to_string_pretty(&self.hosts)?;
@@ -67,7 +72,7 @@ impl Settings {
         let hosts: Vec<HostConfig> = serde_json::from_str(&contents)?;
         Ok(Self {hosts})
     }
-    
+
     pub fn verify_syntax_json(file_path: &str) -> std::io::Result<()> {
         let mut file = File::open(file_path)?;
         let mut contents = String::new();
@@ -81,4 +86,29 @@ impl Settings {
 
         Ok(())
     }
+
+    // yaml
+    pub fn serialize_yaml(&self, file_path: &str) -> std::io::Result<()> {
+        let mut file = File::create(file_path)?;
+        let yaml_str = serde_yaml::to_string(&self.hosts)
+            .map_err(|err| io::Error::new(io::ErrorKind::InvalidData, err))?;
+
+        let formatted_yaml = serde_yaml::to_string(&serde_yaml::from_str::<serde_yaml::Value>(&yaml_str)
+            .map_err(|err| io::Error::new(io::ErrorKind::InvalidData, err))? 
+        ).map_err(|err| io::Error::new(io::ErrorKind::InvalidData, err))?;
+
+        file.write_all(formatted_yaml.as_bytes())?;
+        Ok(())
+    }
+
+    pub fn deserialize_yaml(file_path: &str) -> std::io::Result<Self> {
+        let mut file = File::open(file_path)?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+        let hosts: Vec<HostConfig> = serde_yaml::from_str(&contents)
+            .map_err(|err| io::Error::new(io::ErrorKind::InvalidData, err))?;
+        Ok(Self { hosts })
+    }
+
+    
 }
--- src/tests.rs
+++ src/tests.rs
@@ -4,9 +4,9 @@ use std::path::Path;
 
 #[cfg(test)]
 #[test]
-pub fn test_serialize() {
+pub fn test_serialize_json() {
 
-    let path = "tests/test_host_config";
+    let path = "tests/test.json";
     // first host
     let host1 = config::HostConfig::new(
         "user".to_string(),
@@ -19,12 +19,34 @@ pub fn test_serialize() {
     );
 
     let settings: Settings = Settings::new(vec![host1]);
-    settings.serialize_json("tests/test_host_config").unwrap();
+    settings.serialize_json(path).unwrap();
 }
 
 #[test]
-pub fn test_deserialize() {
-    let path = "hosts";
+pub fn test_deserialize_json() {
+    let path = "tests/test.json";
     let settings = Settings::deserialize_json(path).unwrap();
 }
 
+#[test]
+pub fn test_serialize_yaml() {
+    let path = "tests/test.yaml";
+    let host1 = config::HostConfig::new(
+        "user".to_string(),
+        config::HostIdentifier::Ip(String::from("192.168.1.0/24")),
+        22,
+        Path::new("~/.ssh/testkey").to_path_buf(),
+        Path::new("remote/path").to_path_buf(),
+        Path::new("dest/path").to_path_buf(),
+        24.0,
+    );
+
+    let settings: Settings = Settings::new(vec![host1]);
+    settings.serialize_yaml(path).unwrap();
+}
+
+#[test]
+pub fn test_deserialize_yaml() {
+    let path = "tests/test.yaml";
+    let settings = Settings::deserialize_yaml(path).unwrap();
+}
