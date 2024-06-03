0094615fa084b9deff3266297ab4adcb06f634ef
bampham <cbroo@duck.com>
2024-06-03 12:47:00 +0200

    fixing reading cron

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,4 +1,3 @@
-use rensen_lib::backup::rsync::Sftp;
 use rensen_lib::config::*;
 use rensen_lib::traits::*;
 use rensen_lib::logging::*;
@@ -18,17 +17,23 @@ async fn main() -> Result<(), Trap>  {
         .map_err(|err| Trap::FS(format!("Could not deserialize Global Config: {}", err)))?;
 
     let settings = Settings::deserialize_yaml(&global_config.hosts_path)
-        .map_err(|err| Trap::FS(format!("Could not deserialize Settings @ {:?}: {}", global_config.backupping_path, err)))?;
+        .map_err(|err| Trap::FS(format!("Could not deserialize Settings @ {:?}: {}", global_config.hosts_path, err)))?;
 
     let mut schedules: Vec<Arc<HostSchedule>> = Vec::new();
     for host in settings.hosts.iter() {
         if let Some(cron_schedule) = &host.config.cron_schedule {
+            println!("{:?}", cron_schedule);
             match Schedule::from_str(cron_schedule.as_str()) {
                 Ok(schedule) => {
-                    schedules.push(Arc::new(HostSchedule {
+                    println!("sch: {:?}", schedule);
+                    
+                    let host_schedule = Arc::new(HostSchedule {
                         host: host.clone().into(),
                         schedule,
-                    }));
+                    });
+                    
+                    println!("{:?}", host_schedule);
+                    schedules.push(host_schedule);
                 },
                 Err(err) => {
                     log_trap(&global_config, &Trap::InvalidInput(format!("Invalid Cron Expression for `{}`: {}", host.hostname, err)));
@@ -36,14 +41,24 @@ async fn main() -> Result<(), Trap>  {
             }
         } else {
             log_trap(&global_config, &Trap::Missing(format!("Missing cron_schedule for `{}`: Defaulting to `0 0 * * *`", &host.hostname)));
+            let host_schedule = Arc::new(HostSchedule {
+                host: host.clone().into(),
+                schedule: Schedule::from_str("0 0 * * *").unwrap(),
+            });
+
+            schedules.push(host_schedule);
             continue;
         }
     }
 
-    let backup_scheduler = BackupScheduler::from(Arc::new(global_config), settings, schedules);
+    println!("{:?}", schedules);
+
+    // let backup_scheduler = BackupScheduler::from(Arc::new(global_config), settings, schedules);
+
+    
 
     println!("Starting scheduler ...");
-    backup_scheduler.run_scheduler().await?;
+    //backup_scheduler.run_scheduler().await?;
 
     Ok(())
 }
