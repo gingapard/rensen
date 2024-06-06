118bfce988d8b8557dfb799aa71f24b7e92a7c92
bampham <cbroo@duck.com>
2024-06-06 10:03:16 +0200

    README.md

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
-
-
 # Sentinal Redundancy
 
 Simple remote sync service.
+
+## Build/Install
+
+Install required dependencies:     
+```bash
+sudu ./dep.sh
+```
+
+Run the build script:     
+```bash
+sudo ./build.sh
+```
 
 
--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-sudo apt install libssl-dev
-
 cargo build --manifest-path daemon/Cargo.toml --release
 sudo cp daemon/target/release/rensend /usr/bin/rensend
 sudo cp daemon/service/rensend.service /etc/systemd/system/rensend.service
